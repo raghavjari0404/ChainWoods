f2b7a3168eefc0dac704dc5183689849d6f5250b
raghavjari0404 <raghavjariwala567@gmail.com>
2022-12-10 09:25:33 +0000

    Landing Page Dynamic Data Loader using React Hooks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import CoalTracker from "./abis/woodtracker.json";
+import React, { Component } from "react";
+import Import from "./components/Import";
+import Export from "./components/Export";
+import ConsumerCompany from "./components/ConsumerCompany";
+import Home from "./Home";
+import JsonData from "./data/data.json";
+import Web3 from "web3";
+import { HashRouter, Switch, Route } from "react-router-dom";
+import "./App.css";
+class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			account: "",
+			landingPageData: {},
+		};
+	}
+	getlandingPageData() {
+		this.setState({ landingPageData: JsonData });
+	}
+	async componentDidMount() {
+		this.getlandingPageData();
+	}
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	render() {
+		return (
+			<HashRouter basename="/">
+				<div className="App">
+					<Route path="/" exact component={Home}></Route>
+					<Route path="/Import" component={Import}></Route>
+					<Route path="/Export" component={Export}></Route>
+					<Route
+						path="/ConsumerCompany"
+						component={ConsumerCompany}></Route>
+				</div>
+			</HashRouter>
+		);
+	}
 }
 
 export default App;
